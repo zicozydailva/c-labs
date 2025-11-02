d4624205440d2296bd5b30c70d76dcb047496885
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-11-02 23:01:49 +0100

    chore: data types decl

--- main.cpp
+++ main.cpp
@@ -5,10 +5,13 @@ int main() {// TIP Press <shortcut actionId="RenameElement"/> when your caret is
     auto lang = "C++";
     std::cout << "Hello and welcome to " << lang << "!\n";
 
-    int age = 25;
+    // data type
+    int age = 25; // integer type
+    float height = 1.5; // float type
+    char gender = 'F'; // declaring & initialising a char type
+    double balance = 12345.6789; // double
 
     printf("You are %d years old!\n", age);
 
-    return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
+    return 0;// TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
